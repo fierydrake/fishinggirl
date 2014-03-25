62c87a6a7afcaa610d4cb60fa304751394e42979
mjt <m.j.tunnicliffe@gmail.com>
2014-03-25 13:08:39 +0000

    Fix some stuff with the water Fix fish spawning location

--- src/com/sammik/fishinggirl/Water.java
+++ src/com/sammik/fishinggirl/Water.java
@@ -1,11 +1,54 @@
 package com.sammik.fishinggirl;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Water extends GameObject {
-	public Water(final FishingGirlGame game, final Texture texture, final float x, final float y) {
+	/* 
+	 * These fields define the extent of the water separately from the sprite,
+	 * so that the sprite can be repeated to fill the area. Couldn't find a
+	 * way (that I could understand) to do this with libgdx Texture/Sprite APIs.
+	 */
+	final float widthExtent;
+	final float heightExtent;
+	public Water(final FishingGirlGame game, final Texture texture, final float x, final float y, final float w, final float h) {
 		super(game, texture, x, y);
+		widthExtent = w;
+		heightExtent = h;
+	}
+
+	public float getWidth() { return widthExtent; }
+	public float getHeight() { return heightExtent; }
+	public Rectangle getBoundingRectangle() {
+		return new Rectangle(getX(), getY(), widthExtent, heightExtent);
 	}
 	
-	
+	@Override
+	public void draw(SpriteBatch batch) {
+		final float w = super.getWidth(), h = super.getHeight();
+		if (w == 0f || h == 0f) return;
+		if (w == widthExtent && h == heightExtent) {
+			super.draw(batch);
+		} else {
+			float offsetX = 0f, offsetY = 0f;
+			float remainingWidth = widthExtent, remainingHeight = heightExtent;
+			while (remainingHeight > 0) {
+				// Tile
+				float drawWidth = Math.min(remainingWidth, w);
+				float drawHeight = Math.min(remainingHeight, h);
+				batch.draw(getTexture(), getX() + offsetX, getY() + offsetY, 0, 0, (int)drawWidth, (int)drawHeight);
+				remainingWidth -= drawWidth;
+				offsetX += drawWidth;
+				if (remainingWidth <= 0f) {
+					// reset to left
+					remainingWidth = widthExtent;
+					offsetX = 0f;
+					// move down
+					remainingHeight -= drawHeight; 
+					offsetY += drawHeight;
+				}
+			}
+		}
+	}
 }
